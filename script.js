26b3b3c677e17e4de6dac9add2742ee320601fdd
Phan Quan <hpq833592@gmail.com>
2023-10-10 00:02:27 +0700

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const adressBtn = document.querySelector('#adress-form')
 const adressClose = document.querySelector('#adress-close')
-
+let index = 0
 
 adressBtn.addEventListener("click",function(){
     const adressForm = document.querySelector('.adress-form');
@@ -10,3 +10,21 @@ adressClose.addEventListener("click",function(){
     const adressForm  = document.querySelector('.adress-form');
     adressForm.style.display = "none";
 });
+// slider-----------------------------------------------------
+const rightbtn = document.querySelector('.bx-chevron-right');
+const leftbtn = document.querySelector('.bx-chevron-left')
+const imgNumber = document.querySelectorAll('.slider-content-left-top img')
+rightbtn.addEventListener("click",function(){
+    index = index+1 
+    if(index > imgNumber.length-1){
+        index = 0
+    }
+    document.querySelector(".slider-content-left-top").style.right = index *100+"%"
+})
+leftbtn.addEventListener("click",function(){
+    index = index-1 
+    if(index < 0){
+        index = imgNumber.length-1
+    }
+    document.querySelector(".slider-content-left-top").style.right = index *100+"%"
+})
