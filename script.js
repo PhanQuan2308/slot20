393d10b78a6c6b44dc9d8a5ae6e51cd93c0eeea2
Phan Quan <hpq833592@gmail.com>
2023-11-02 16:52:21 +0700

    update

--- script.js
+++ script.js
@@ -1,38 +1,94 @@
-const adressBtn = document.querySelector('#adress-form')
-const adressClose = document.querySelector('#adress-close')
-let index = 0
+document.addEventListener("DOMContentLoaded", function () {
+    let slideIndex = 0;
+    showSlides();
 
-adressBtn.addEventListener("click",function(){
-    const adressForm = document.querySelector('.adress-form');
-    adressForm.style.display = "flex";
-});
-adressClose.addEventListener("click",function(){
-    const adressForm  = document.querySelector('.adress-form');
-    adressForm.style.display = "none";
-});
-// slider-----------------------------------------------------
-const rightbtn = document.querySelector('.bx-chevron-right');
-const leftbtn = document.querySelector('.bx-chevron-left')
-const imgNumber = document.querySelectorAll('.slider-content-left-top img')
-rightbtn.addEventListener("click",function(){
-    index = index+1 
-    if(index > imgNumber.length-1){
-        index = 0
-    }
-    document.querySelector(".slider-content-left-top").style.right = index *100+"%"
-})
-leftbtn.addEventListener("click",function(){
-    index = index-1 
-    if(index < 0){
-        index = imgNumber.length-1
-    }
-    document.querySelector(".slider-content-left-top").style.right = index *100+"%"
-})
-// slider------------------------------------------
-const imgNumberLi = document.querySelectorAll('.slider-content-left-bottom li')
-imgNumberLi.forEach(function(image,index){
-    image.addEventListener("click",function(){
+    function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); 
 
-        document.querySelector(".slider-content-left-top").style.right = index *100+"%"
-    })
-})
+}
+
+$('.staff-down').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    setTimeout: 3000,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='bx bx-chevron-left' ></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='bx bx-chevron-right' ></i></button>"
+  });
+  $('.clients-down-right').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    setTimeout: 3000,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='bx bx-chevron-left' ></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='bx bx-chevron-right' ></i></button>"
+  });
+  function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+  const name = document.getElementById('text');
+  const email = document.getElementById('email');
+  const form  = document.getElementById('form');
+  const errorElement = document.getElementById('error');
+  form.addEventListener('submit', (e) => {
+    let messages = []
+    if(name.value === '' || name.value == null){
+      messages.push('Name is required')
+    }
+    
+    if(messages.length >0){
+      e.preventDefault()
+      errorElement.innerHTML = messages.join(', ')
+    }
+  })
+
+
+
+// ---------------location time------------
+  function updateDateTimeLocation() {
+    var currentDate = new Date();
+    document.getElementById('date').innerHTML = currentDate.toDateString();
+    document.getElementById('time').innerHTML = currentDate.toLocaleTimeString();
+    
+    document.getElementById('location').innerHTML = 'Ha Noi';
+  }
+  setInterval(updateDateTimeLocation, 1000);
+  updateDateTimeLocation();
+  function getVisitorCount() {
+    return Math.floor(Math.random() * 1000); 
+}
+
+// --------------visitor-------------
+function displayVisitorCount() {
+    var visitorCount = getVisitorCount();
+    var countElement = document.getElementById('visitor-count');
+    countElement.innerText = 'Visitors: ' + visitorCount;
+}
+
+displayVisitorCount();
+const settings = require('./setting.json');
+
+
+});
